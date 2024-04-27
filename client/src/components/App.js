67f31bb627033ce4dcc59afe6333f4ec7be56179
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-04-26 23:23:41 -0400

    added respective components to the nav and added routes for them

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./Homepage"
+import About from "./About"
+import Services from "./Services";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 function App() {
   return <>
     <Switch>
       <Route exact path="/">
         <Homepage></Homepage>
+      </Route>
+      <Route exact path="/about">
+        <About></About>
+      </Route>
+      <Route exact path="/services">
+        <Services></Services>
+      </Route>
+      <Route exact path="/projects">
+        <Projects></Projects>
+      </Route>
+      <Route exact path="/contact">
+        <Contact></Contact>
       </Route>
     </Switch>
   </>
