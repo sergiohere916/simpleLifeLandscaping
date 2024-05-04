fb5d46ce77e074d61c0aeb4ed9621e232555deca
Sergio Heredia <sergioheredia1996@gmail.com>
2024-05-03 22:04:12 -0700

    adding services images

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,12 +1,34 @@
 import React from "react";
-import Header from "./Header"
+import Header from "./Header";
+import mowerImg from "../images/licensed_mower.jpeg";
+import shovelImg from "../images/licensed_snowshovel.jpeg";
+import grassImg from "../images/licensed_grass.jpeg";
+import plantImg from "../images/licensed_plant.jpeg";
 
 function Services() {
-    return <div>
+    return <div id="servicesPage">
         <Header></Header>
         <div id="service-intro">
+            <div id="service-images">
+                <div className="service-img">
+                    <img src={mowerImg}/>
+                </div>
+                <div className="service-img">
+                    <img src={shovelImg}/>
+                </div>
+                <div className="service-img">
+                    <img src={grassImg}/>
+                </div>
+                <div className="service-img">
+                    <img src={plantImg}/>
+                </div>
+            </div>
             <div id="intro-title">Simple Life Landscaping Services</div>
-            <div id="intro-details">Professional Services for you</div> 
+            <div id="intro-details">Professional Services for you in Columbus Ohio region</div>
+            {/* <div id="main-service-img">
+                <img src={plantImg} />
+            </div> */}
+            <div style={{marginTop: "2%"}}>"We provide our clientel with "</div> 
         </div>
         <div id="services-provided">
             <div id="service1">
