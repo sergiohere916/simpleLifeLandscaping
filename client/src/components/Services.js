a56def4165fbbe77c3a5ee0abb50d28cb6f63133
Sergio Heredia <sergioheredia1996@gmail.com>
2024-05-05 23:42:59 -0700

    added content to services page

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -4,6 +4,8 @@ import mowerImg from "../images/licensed_mower.jpeg";
 import shovelImg from "../images/licensed_snowshovel.jpeg";
 import grassImg from "../images/licensed_grass.jpeg";
 import plantImg from "../images/licensed_plant.jpeg";
+import serviceImg1 from "../images/simple_life_06.jpg";
+import serviceImg2 from "../images/simple_life_02.jpg";
 
 function Services() {
     return <div id="servicesPage">
@@ -28,17 +30,53 @@ function Services() {
             {/* <div id="main-service-img">
                 <img src={plantImg} />
             </div> */}
-            <div style={{marginTop: "2%"}}>"We provide our clientel with "</div> 
+            <div id="intro-quote">"We provide our clients with quality work taking each project into careful consideration producing a unique style that will bring out the best of the particular landscape"</div> 
         </div>
         <div id="services-provided">
-            <div id="service1">
-                <div id="services-title">Professional Services Provided:</div>
-                <ul id="service-list">
-                    <li>Lawn Mowing</li>
-                    <li>Edging</li>
-                    <li>Trimming</li>
-                    <li>Mulching</li>
-                </ul>
+            <div id="service1-content">
+                <div id="service1-img">
+                    <img src={serviceImg1} alt="service1img"/>
+                </div>
+                <div id="service1">
+                    <div id="services-title">Professional Services Provided:</div>
+                    <ul id="service-list">
+                        <li>Lawn Mowing</li>
+                        <li>Edging</li>
+                        <li>Trimming</li>
+                        <li>Mulching</li>
+                        <li>Landscape re-design</li>
+                        <li>Plant Installation</li>
+                        <li>Transplants</li>
+                        <li>Snow removal</li>
+                        <li>Leaf blwoing</li>
+                        <li>Seeding</li>
+                        <li>Sodding</li>
+                    </ul>
+                    <button>Contact Us</button>
+                </div>   
+            </div>
+            <div id="service1-content">
+                <div id="service1-img">
+                    <img src={serviceImg2} alt="service1img"/>
+                </div>
+                <div id="service1">
+                    <div id="services-title">Professional Services Provided:</div>
+                    <ul id="service-list">
+                        <li>Lawn Mowing</li>
+                        <li>Edging</li>
+                        <li>Trimming</li>
+                        <li>Mulching</li>
+                        <li>Landscape re-design</li>
+                        <li>Plant Installation</li>
+                        <li>Transplants</li>
+                        <li>Snow removal</li>
+                        <li>Leaf blwoing</li>
+                        <li>Seeding</li>
+                        <li>Sodding</li>
+                    </ul>
+                    <button>Contact Us</button>
+                </div>
+                
             </div>
         </div>
     </div>
