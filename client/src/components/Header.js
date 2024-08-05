fb4880e138ca546a8883d8fec3690112245425d4
Sergio Heredia <sergioheredia1996@gmail.com>
2024-08-04 21:12:01 -0700

    css updates

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,33 +1,20 @@
 import React from "react"
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import "../css/Header.css"
-
 import simpleLifeLogo from "../images/sl_Logo.png";
 
 function Header({showHiddenNav}) {
 
 
     return (
-    // <div className="header-container">
-    //     <div className="logo-container">
-    //         Simple Life Landscaping
-    //     </div>
-    //     <div className="navlink-container">
-    //         <NavLink to="/">Home</NavLink>
-    //         <NavLink to="about">About</NavLink>
-    //         <NavLink to="/services">Services</NavLink>
-    //         <NavLink to="projects">Projects</NavLink>
-    //         <NavLink to="/contact">Contact</NavLink>
-    //     </div>
-    // </div>
     <div id="nav2">
-            <div className="logo-container2">
+            <div className="logo-container">
                 <div id="main-logo">
                     <img src ={simpleLifeLogo} alt="simple-life-logo"/>    
                 </div>    
                 <div id="company-title">Simple Life Landscaping</div>
             </div>
-            <div className="navlink-container2">
+            <div className="navlink-container">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="about">About</NavLink>
                 <NavLink to="/services">Services</NavLink>
